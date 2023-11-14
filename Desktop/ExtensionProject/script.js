6659040ec775200f52615841fc6b528d8a23de06
CarolSam <anacarolfuentes@hotmail.com>
2023-11-13 23:24:17 -0300

    Estilização do Tabuleiro

---
+++ Desktop/ExtensionProject/script.js
@@ -0,0 +1,4 @@
+
+/*var perguntas = ['Pergunta 1', 'Pergunta 2', 'Pergunta 3', 'Pergunta 4'];
+var random = perguntas[Math.floor(Math.random() * perguntas.length)];
+document.write("<h1>" + random + "</h1>"); */
