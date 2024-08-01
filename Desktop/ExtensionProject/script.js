3127c9ce0497f1e53f2f09f44f29a71383056dec
Ana Carolina Fuentes <103052499+AnaCarolFuentes@users.noreply.github.com>
2024-07-31 22:04:23 -0300

    Delete Desktop/ExtensionProject directory

--- Desktop/ExtensionProject/script.js
+++
@@ -1,4 +0,0 @@
-
-/*var perguntas = ['Pergunta 1', 'Pergunta 2', 'Pergunta 3', 'Pergunta 4'];
-var random = perguntas[Math.floor(Math.random() * perguntas.length)];
-document.write("<h1>" + random + "</h1>"); */
